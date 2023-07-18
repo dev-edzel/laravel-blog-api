0d2ff172baf8bd2cf593e9a04064654933e1a2ec
Edzel <dev.edzeldacanay@gmail.com>
2023-07-18 13:05:51 +0800

    adding task

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/tasks', [TaskController::class, 'store']);
     Route::put('/tasks/{task}', [TaskController::class, 'update']);
     Route::delete('/tasks/{task}', [TaskController::class, 'delete']);   
-
+ 
     });
 
 Route::post('/signup', [AuthControlller::class, 'sign_up']);
