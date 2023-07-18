0099d9272561783fe019bc3ef34db31751c218ea
Edzel <dev.edzeldacanay@gmail.com>
2023-07-18 11:54:46 +0800

    adding controller

--- database/migrations/2023_07_18_035256_create_tasks_table.php
+++ database/migrations/2023_07_18_035256_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('due_date');
+            $table->string('status');
             $table->timestamps();
         });
     }
