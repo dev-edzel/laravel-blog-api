4ca078759f76af49e637666a85c2d49d685abb1c
Edzel <dev.edzeldacanay@gmail.com>
2023-07-20 16:25:48 +0800

    updated

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,56 +8,54 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store(Request $request, $id)
-{
-    // Validate data
-    $data = $request->validate([
-        'content' => 'required'
-    ]);
+    public function store(Request $request, $postId)
+    {
+        $post = Post::find($postId);
+        if(!$post) {
+            return response()->json(['error' => 'Post not found'], 404);
+        }
 
-    $post = Post::findOrFail($id);
-    $comment = new Comment($data);
-    $comment->user_id = auth()->id();
-    $post->comments()->save($comment);
+        $comment = new Comment();
+        $comment->content = $request->input('content');
+        $comment->user_id = auth()->id();
+        $post->comments()->save($comment);
 
-    return response()->json($comment, 201);
-}
-
-public function index($id)
-{
-    $post = Post::findOrFail($id);
-    return response()->json($post->comments);
-}
-
-public function update(Request $request, $id)
-{
-    // Validate data
-    $data = $request->validate([
-        'content' => 'required'
-    ]);
-
-    $comment = Comment::findOrFail($id);
-
-    // Ensure the user updating the comment is the author
-    if ($comment->user_id !== auth()->id()) {
-        return response()->json(['error' => 'Unauthorized'], 403);
+        return response()->json($comment);
     }
 
-    $comment->update($data);
-    return response()->json($comment);
-}
+    public function show($postId)
+    {
+        $post = Post::find($postId);
+        if(!$post) {
+            return response()->json(['error' => 'Post not found'], 404);
+        }
 
-public function destroy($id)
-{
-    $comment = Comment::findOrFail($id);
-
-    // Ensure the user deleting the comment is the author
-    if ($comment->user_id !== auth()->id()) {
-        return response()->json(['error' => 'Unauthorized'], 403);
+        return response()->json($post->comments);
     }
 
-    $comment->delete();
-    return response()->json(null, 204);
-}
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::find($id);
+        if(!$comment || $comment->user_id !== auth()->id()) {
+            return response()->json(['error' => 'Comment not found or not authorized'], 403);
+        }
+
+        $comment->content = $request->input('content');
+        $comment->save();
+
+        return response()->json($comment);
+    }
+
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        if(!$comment || $comment->user_id !== auth()->id()) {
+            return response()->json(['error' => 'Comment not found or not authorized'], 403);
+        }
+
+        $comment->delete();
+
+        return response()->json(['message' => 'Comment deleted']);
+    }
 }
 
